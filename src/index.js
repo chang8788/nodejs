273906e6485c67d8a0fcc471ab123409042e26de
Thu Trang <changchang8788@gmail.com>
2023-11-08 08:36:23 +0700

    Template engine-handlebars

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+//const handlebars = require('express-handlebars');
+const { engine } = require ('express-handlebars');
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', handlebars());
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
 
 app.get('/', (req, res) => {
- 
   res.render('home');
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/news', (req, res) => {
+  res.render('news');
 });
+app.listen(3000);
+
+//app.listen(port, () => {
+  //console.log(`Example app listening on port ${port}`)
+//});
